1349b8ff7f8b29c6c77360b534546ac776434e09
kevin68107 <kevinaihoon16@gmail.com>
2024-05-06 13:48:09 -0700

    "Updated the string data type file"

--- string-data-type.py
+++ string-data-type.py
@@ -1,4 +1,21 @@
+###Introducing the string data type#
+
 myString = "This is a string."
 print(myString)
 print(type(myString))
 print(myString + " is of the data type " + str(type(myString)))
+
+##Working with string concatenation
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+#Working with input string
+name = input("what is your name? ")
+print(name)
+
+#Formating output strings
+color = input("what is your favorite color? ")
+animal = input("what is your favorite animal? ")
+print("{}, you like a {} {}!".format(name, color, animal))
